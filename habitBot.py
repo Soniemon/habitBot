4dd543885940ba86ce1498a4e12e99f0e965f061
khoiness <khoicheng@sbcglobal.net>
2024-11-16 17:52:58 -0600

    Added a command bot to implement the /showCommands command

--- habitBot.py
+++ habitBot.py
@@ -2,6 +2,7 @@
 
 import os
 import discord
+from discord.ext import commands #Implements detection and functionality for / commands
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,29 +12,47 @@ GUILD = os.getenv('DISCORD_GUILD')
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+# @client.event
+# async def on_ready():
+#     for guild in client.guilds:
+#         if guild.name == GUILD:
+#             break
 
 
-    print(f'{client.user} is connected to the following guild:\n'
-          f'{guild.name}(id: {guild.id})'
-    )
+#     print(f'{client.user} is connected to the following guild:\n'
+#           f'{guild.name}(id: {guild.id})'
+#     )
     
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+#     members = '\n - '.join([member.name for member in guild.members])
+#     print(f'Guild Members:\n - {members}')
 
 
-@client.event
-async def on_message(message):
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+
+#     if 'hello' in message.content.lower():
+#         await message.channel.send(message.author.display_name)
+
+guildBot = commands.Bot(command_prefix="/", intents = discord.Intents.all()) #Flags anything beginning with a / as a command
+
+@guildBot.command(name="showCommands") #Behavior when /help is detected in chat
+async def help(ctx): #Ctx is a required parameter that contains information about the message, including the channel and guild and user that called the command
+
     if message.author == client.user:
         return
 
-    if 'hello' in message.content.lower():
-        await message.channel.send(message.author.display_name)
+    response = """/showGrid --> Displays user's personal habit grid \n\n----------------------------------------\n
+    /newRemind --> Set habit reminder timing \n\n----------------------------------------\n
+    /changeRemind --> Adjust existing reminder timing \n\n----------------------------------------\n
+    /showShared --> Show shared habit grid \n\n----------------------------------------\n
+    /newHabit --> Set new habit \n\n----------------------------------------\n
+    /deleteHabit --> Delete existing habit \n\n----------------------------------------\n
+    /showStreak --> Show habit streaks \n\n----------------------------------------\n"""
+
+    await ctx.send(response)
 
 
-
-client.run(TOKEN)
+guildBot.run(TOKEN)
+#client.run(TOKEN)
